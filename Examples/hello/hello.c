852744b336eb5208662489671400697eb2c297be
Mallesh Koujalagi <mallesh.koujalagi@gmail.com>
2015-05-05 23:31:06 -0500

    Example:Hello My first hello world module
    To explore linux internal, started with small step, let us c!!!!

---
+++ Examples/hello/hello.c
@@ -0,0 +1,24 @@
+#include<linux/init.h>
+#include<linux/module.h>
+
+
+
+static int __init hello_init(void)
+{
+
+	printk(KERN_ALERT "Hello World, My fist module epxlorer example\n");
+	return 0;
+}
+
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_ALERT "Good Bye world\n");
+
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("Mallesh K");
+MODULE_LICENSE("GPL");
