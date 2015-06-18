f6014a53d5d090f3a5034a7da720a17c33c81051
Mallesh Koujalagi <mallesh.koujalagi@gmail.com>
2015-06-17 21:12:08 -0500

    char_dev0:Experiment with character device
    Allocated character device and checked it present using
    cat /proc/devices | grep my_cdev command
    
    Signed-off-by: Mallesh Koujalagi <mallesh.koujalagi@gmail.com>

---
+++ Examples/char_dev0/chardev0.c
@@ -0,0 +1,35 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/version.h>
+
+
+static dev_t device_no;
+
+static __init int my_cdev_init(void)
+{
+	printk(KERN_ALERT "Enter character dev\n");
+	if(alloc_chrdev_region(&device_no, 0,5,"my_cdev") < 0){
+		printk(KERN_ALERT "Failed to allocate character device\n");
+		return -1;
+	}
+	printk(KERN_ALERT "Hey dude\n");
+	MKDEV(MAJOR(device_no), MINOR(device_no));
+	printk(KERN_ALERT "<Major Number:Minor Number><%d:%d>\n", MAJOR(device_no), MINOR(device_no));
+	return 0;
+}
+
+static __exit void my_cdev_exit(void)
+{
+	unregister_chrdev_region(device_no, 5);
+	printk(KERN_ALERT "Bye Bye\n");
+
+}
+
+module_init(my_cdev_init);
+module_exit(my_cdev_exit);
+
+MODULE_AUTHOR("Mallesh Koujalagi");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Experiment on charactor device \n");
