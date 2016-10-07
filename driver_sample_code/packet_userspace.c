28e6a1fb52c563cd1358f7878d6da83f629d79ea
Mallesh Koujalagi <mallesh.koujalagi@gmail.com>
2016-10-07 06:18:16 -0700

    Added interesting link, to explore linux world
    Signed-off-by: Mallesh Koujalagi <mallesh.koujalagi@gmail.com>

---
+++ driver_sample_code/packet_userspace.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
---
+++ user_space_driver_model/kpart.c
@@ -0,0 +1,5 @@
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/uio_driver.h>
+
+
